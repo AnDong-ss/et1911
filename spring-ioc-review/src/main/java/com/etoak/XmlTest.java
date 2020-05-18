863bd5b93f76c721311c9f418c0d9c08fcdebde1
et1911 <et1911@etoak.com>
2020-05-18 15:48:50 +0800

    提交spring-ioc-review

---
+++ spring-ioc-review/src/main/java/com/etoak/XmlTest.java
@@ -0,0 +1,17 @@
+package com.etoak;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.etoak.action.UserAction;
+import com.etoak.bean.User;
+
+public class XmlTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("spring.xml");
+		UserAction userAction = ioc.getBean(UserAction.class);
+		User user = userAction.getById(1000);
+		System.out.println(user.getId());
+	}
+}
